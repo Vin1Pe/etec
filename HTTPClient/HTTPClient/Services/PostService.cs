7a8c3b0e6b5d8d6b0677543baf4d733d26374d0f
Vin1Pe <viniciuspelissari2707@gmail.com>
2024-09-11 18:13:16 -0300

    feat: HTTPClient project

---
+++ HTTPClient/HTTPClient/Services/PostService.cs
@@ -0,0 +1,27 @@
+﻿using HTTPClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace HTTPClient.Services
+{
+    public class PostService
+    {
+        private HttpClient httpClient;
+        private Post post;
+        private List<Post> posts;
+        private JsonSerializerOptions jsonSerializerOptions;
+        public PostService()
+        {
+            httpClient = new HttpClient();
+            jsonSerializerOptions = new JsonSerializerOptions{
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
+        }
+
+    }
+}
---
+++ HTTPClient/HTTPClient/ViewModels/PostViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using HTTPClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTPClient.ViewModels
+{
+    public partial class PostViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        List<Post> posts;
+
+        public void getPost()
+        {
+
+        }
+    }
+}
